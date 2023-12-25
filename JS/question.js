68d36b285180102ad10bc1c0a9da2dbac6cd394c
Jose <147728854+jose-1804@users.noreply.github.com>
2023-12-24 21:10:40 -0400

    Jose-growth

---
+++ JS/question.js
@@ -0,0 +1,22 @@
+(function(){
+    const titleQuestion = [...document.querySelectorAll('.question_title')];
+    console.log(titleQuestion)
+
+        titleQuestion.forEach(question =>{
+            question.addEventListener('click',()=>{
+                let height = 0
+                let answer = question.nextElementSibling;
+                let addPadding =question.parentElement.parentElement;
+
+
+                addPadding.classList.toggle('question_padding--add')
+                question.children[0].classList.toggle('question_arrow--rotate')
+
+                if(answer.clientHeight === 0){
+                    height = answer.scrollHeight;
+                }
+
+                answer.style.height = `${height}px`;
+            });
+        })
+})();
